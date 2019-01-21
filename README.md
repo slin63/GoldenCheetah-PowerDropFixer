911dc2ab6bac1462fec1f3bb13675bd26e7db1ce
arfenarf <kate@kateweber.com>
2019-01-21 11:22:17 -0500

    Added Heart Rate smoothing and minor spelling tweaks

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # GoldenCheetah-PowerDropFixer
 
-Fixes power gaps from signal drops that can't be fixed using GoldenCheetah's fix gaps in recording tool.
+Fixes power, cadence, and heart-rate gaps from signal drops that can't be fixed using GoldenCheetah's fix gaps in recording tool.
 
 Mirrors the tolerance and stop settings in GC's fix gaps in recording tool.`Tolerance` defines minimum length of a signal drop to fix (inclusive). `Stop` defines maximum length of a signal drop to fix (exclusive).
 
 Example usage:
 -----
 
-Let's say your power meter usually drops signal for 3 - 6 seconds. We can target these 3 - 6 second drops by setting `TOLERANCE=2` and `STOP=7` in the actual .py file (constants are listed in all caps at the top)
+Let's say your power meter usually drops signal for 3 - 6 seconds. We can target these 3 - 6 second drops by setting `TOLERANCE=2` and `STOP=7` in the actual .py file (constants are listed in all caps at the top).
+Cadence and HR values will be repaired as well, with the assumption that they dropped out at the same time power did.
 
-If you want to preview what the script will do to your activity file: `$python3 fixPower.py 2018_03_26_10_16_14.json --no-save`
+The script assumes you're running it from your GC activities driectory. If you want to preview what the script will do to your activity file: `$python3 fixPower.py 2018_03_26_10_16_14.json --no-save`
 
 If you want to save the proposed changes: `$python3 fixPower.py 2018_03_26_10_16_14.json --save`
 
--- fixPower.py
+++ fixPower.py
@@ -1,9 +1,12 @@
 """
-Fixes power/cadence meter drop outs by filling in zero values with
-the average of the last value before the signal dropped and the first
+Fixes power/cadence/heart rate meter drop outs by filling in zero values with
+the average of the last value before the power signal dropped and the first
 value that appears when the signal is reacquired.
 
 Will produce inaccurate data for anything other than interval exercises.
+
+If Cadence or HR aren't dropping on exactly the same rhythm as the power data,
+you may get inaccurate fixes on those values
 """
 
 """
@@ -19,8 +22,6 @@ import os
 import argparse
 from pprint import pprint
 
-DIRECTORY_ACTIVITY = '/Users/sSDSD/Library/GoldenCheetah/shean/activities'
-FILE_TEST = '/Users/sSDSD/Library/GoldenCheetah/shean/activities/2018_03_26_10_16_14.json'
 TOLERANCE = 0 # <int> Minimum time gap to fill (exclusive)
 STOP = 12     # <int> Maximum time gap to fill (inclusive)
 
@@ -56,13 +57,13 @@ def _output_json(data, output):
 
 def _fill_zeroes(timeseries):
     """
-    Counts number of consequentive zero power instances.
-    When zero power instances exceed the tolerance but sit below the stop
+    Counts number of consecutive zero value instances.
+    When zero value instances exceed the tolerance but sit below the stop
     those values are overwritten with the average of the last and first signals received.
     """
     index = 0     # Current place we're moving through in the sample
-    zero_power_index = 0 # First occurance of zero power
-    zero_power_count = 0 # Consequetive instances of zero power
+    zero_power_index = 0  # First occurrence of zero power
+    zero_power_count = 0  # Consecutive instances of zero power
 
     power_last = 0       # Last power reading before signal lost
     power_first = 0      # First power reading when signal returns
@@ -70,27 +71,33 @@ def _fill_zeroes(timeseries):
     cad_last = 0         # Last cadence reading before signal lost
     cad_first = 0        # First cadence reading when signal returns
 
+    hr_last = 0  # Last heart rate reading before signal lost
+    hr_first = 0  # First heart rate reading when signal returns
+
     samples_to_fill = 0  # Number of power samples to fill
     fill_value_pow = 0   # Power number to fill with
     fill_value_cad = 0   # Cadence number to fill with
+    fill_value_hr = 0  # Heart rate number to fill with
 
     while index < len(timeseries):
         # print('INDEX=', index)
 
         power_current = timeseries[index]['WATTS']
         cadence_current = timeseries[index]['CAD']
+        hr_current = timeseries[index]['HR']
 
-        # Capture the last power/cad reading before power turns to zero
+        # Capture the last power/cad/hr reading before power turns to zero
         if index > 0 and timeseries[index - 1]['WATTS'] != 0 and power_current == 0:
 
             power_last = timeseries[index - 1]['WATTS']
             cad_last = timeseries[index - 1]['CAD']
+            hr_last = timeseries[index - 1]['HR']
 
         # If not repairing data, track signal loss & develop fill values
         if samples_to_fill == 0:
             # Record the number of instances of zero power
             if power_current == 0:
-                if zero_power_index == 0:    # If this is the first instance of zero power, record the occurance
+                if zero_power_index == 0:  # If this is the first instance of zero power, record the occurrence
                     zero_power_index = index
 
                 zero_power_count += 1        # Increment zero power count
@@ -102,14 +109,17 @@ def _fill_zeroes(timeseries):
                 if TOLERANCE < zero_power_count <= STOP:
                     power_first = power_current
                     cad_first = cadence_current
+                    hr_first = hr_current
+
                     samples_to_fill = zero_power_count
 
                     # Calculate fill values for power/cadence
                     fill_value_cad = __average_two_vals(cad_last, cad_first)
                     fill_value_pow = __average_two_vals(power_last, power_first)
+                    fill_value_hr = __average_two_vals(hr_last, hr_first)
 
-                    console_log = "Fixing {0} samples with\n\tPOWER: {1}\n\tCAD: {2}\n\t"
-                    print(console_log.format(samples_to_fill, fill_value_pow, fill_value_cad))
+                    console_log = "Fixing {0} samples with\n\tPOWER: {1}\n\tCAD: {2}\n\tHR: {3}\n\t"
+                    print(console_log.format(samples_to_fill, fill_value_pow, fill_value_cad, fill_value_hr))
 
                     # Set current index to the first index that needs repairing
                     index = zero_power_index
@@ -121,6 +131,7 @@ def _fill_zeroes(timeseries):
 
                     power_last = 0
                     cad_last = 0
+                    hr_last = 0
 
 
 
@@ -128,6 +139,7 @@ def _fill_zeroes(timeseries):
         else:
             timeseries[index]['WATTS'] = fill_value_pow
             timeseries[index]['CAD'] = fill_value_cad
+            timeseries[index]['HR'] = fill_value_hr
             samples_to_fill -= 1
 
             # Once we've repaired all required samples, reset counter values and apply initial filling
@@ -135,6 +147,7 @@ def _fill_zeroes(timeseries):
 
                 timeseries[zero_power_index]['WATTS'] = fill_value_pow
                 timeseries[zero_power_index]['CAD'] = fill_value_cad
+                timeseries[zero_power_index]['HR'] = fill_value_hr
 
                 zero_power_index = 0
                 zero_power_count = 0
@@ -145,16 +158,20 @@ def _fill_zeroes(timeseries):
                 cad_last = 0
                 cad_first = 0
 
+                hr_last = 0
+                hr_first = 0
+
                 samples_to_fill = 0
                 fill_value_pow = 0
                 fill_value_cad = 0
+                fill_value_hr = 0
 
         index += 1
 
 
 def __average_two_vals(a, b):
     print('AVG OF {0}, {1} = {2}'.format(a, b, ((a + b) / 2.0)))
-    return (a + b) / 2.0
+    return round((a + b) / 2.0, 0)  # GC Doesn't like fractional values
 
 
 def __full_file_name(file_name):
@@ -174,3 +191,4 @@ if __name__ == '__main__':
 
     print('RUNNING on {}'.format(json_file))
     fix_zeroes(json_file, args.save)
+
